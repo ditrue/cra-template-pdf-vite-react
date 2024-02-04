e994e04c115685ba71e106aec4b3d656502dcb4c
pandifeng <pandifeng@thape.com.cn>
2024-02-04 18:13:49 +0800

    ts 配置

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -2,13 +2,26 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 
-// https://vitejs.dev/config/
+// 确保导入 dotenv 并调用 config 方法
+// 这一步在 Vite 项目中通常是可选的，因为 Vite 默认会加载 .env 文件
+import dotenv from "dotenv"
+dotenv.config()
+
 export default defineConfig({
-  base: "/api/micro-app/portal/",
+  base: process.env.BASENAME,
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_BASE_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
     },
   },
 })

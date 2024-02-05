215f8e54eb07457a077277aefce43f4c84449ce5
pandifeng <pandifeng@thape.com.cn>
2024-02-05 09:56:53 +0800

    修改路由配置

--- template/src/vite-env.d.ts
+++ template/src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  // 定义所有你期望的环境变量
-  readonly VITE_API_BASE_URL: string
-  // 更多环境变量...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
